1f8d9be816858e6a063a21401a79f11d43a3e253
yami <yami-rnd@skvip.org>
2026-01-08 16:33:28 +0800

    Fix Gradle plugin ClassLoader conflicts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,12 @@
 plugins {
-    // Shared plugins can be defined here
+    alias(libs.plugins.kotlin.multiplatform) apply false
+    alias(libs.plugins.kotlin.serialization) apply false
+    alias(libs.plugins.android.library) apply false
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.jetbrains.compose) apply false
+    alias(libs.plugins.compose.compiler) apply false
 }
 
 allprojects {
     // Repositories are defined in settings.gradle.kts
 }
-
