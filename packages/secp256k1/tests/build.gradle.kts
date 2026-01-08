7e2a3d7d3f46d610491c37b6dc1d0d6d81ffcd25
ImL1s <aa22306546@hotmail.com>
2026-01-08 22:44:35 +0800

    fix: apply android plugin to secp256k1 tests

--- packages/secp256k1/tests/build.gradle.kts
+++ packages/secp256k1/tests/build.gradle.kts
@@ -7,6 +7,7 @@ import org.jetbrains.kotlin.gradle.targets.native.tasks.KotlinNativeSimulatorTes
 
 plugins {
     alias(libs.plugins.kotlin.multiplatform)
+    alias(libs.plugins.android.library)
 }
 
 repositories {
